a1486d37b221685ae700420e5fd0e7d575ef66a8
Nporci16 <nicolas.porci@gmail.com>
2022-06-06 20:44:55 -0300

    proyecto tenis

---
+++ index.js
@@ -0,0 +1,21 @@
+let entrada = prompt ("Ingrese el producto que esta buscando")
+while (entrada != null){
+    switch (entrada) {
+        case "remera" || "Remera":
+            alert("Tenemos remeras.");
+            entrada = null;
+            break;
+        case "pantalon" || "Pantalon" || "pantalones" || "Pantaol":
+            alert("Tenemos pantalones. ");
+            entrada = null;
+            break;
+        case "gorra" || "gorro" || "Gorra" || "Gorro":
+            alert("Tenemos gorras.");
+            entrada = null;
+            break;
+        default: 
+            alert("no tenemos ese producto");
+            entrada = null;
+            break;
+    }
+}
